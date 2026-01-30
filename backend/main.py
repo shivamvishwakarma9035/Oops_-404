46e4b386e60bf16093c43be293366585fb002c9a
Shivam <Shivam.2024ug3009@iiitranchi.ac.in>
2026-01-30 12:56:18 +0530

    setting up fast api

--- backend/main.py
+++ backend/main.py
@@ -2,3 +2,20 @@ from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
 import json
 import pyautogui
+
+app = FastAPI()
+
+# CORS Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+ALPHA = 0.4
+prev_x, prev_y = 0.5, 0.5
+pyautogui.FAILSAFE = False
+
+SCREEN_WIDTH, SCREEN_HEIGHT = pyautogui.size()
+
